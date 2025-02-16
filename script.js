40e60441ba8ba55922b9fba7a9393c7e1aeb577e
kevenscha <kevenscha@gmail.com>
2025-02-16 00:58:41 -0300

    Alterado novamente o projeto para uma versão antiga, e criado um botão switch para mudança de tema claro e escuro do site

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleButton() {
+    let html = document.documentElement;
+    let button = document.getElementById("phase");
+
+    if (html.classList.contains("lightMode")){
+        html.classList.remove("lightMode");
+        button.src = "./src/assets/toggle-sun.png";
+    } else {
+        html.classList.add("lightMode");
+        button.src = "./src/assets/toggle.png";
+    }
+}
